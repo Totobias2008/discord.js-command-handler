b010372089abae75b9a10c9042f2ba6b53978787
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-01-04 23:05:36 +0530

    Update slash_handler.js

--- handlers/slash_handler.js
+++ handlers/slash_handler.js
@@ -7,35 +7,48 @@ const fs = require('fs');
    */
 
 module.exports = (client) => {
-    try {
-        let command = 0;
-        const arrayOfCommands = [];
-        fs.readdirSync("./Commands").forEach(cmd => {
-            let commands = fs.readdirSync(`./Commands/${cmd}/`).filter((file) => file.endsWith(".js"));
-            for (cmds of commands) {
-                let pull = require(`../Commands/${cmd}/${cmds}`);
-                if (pull.name) {
-                    client.Commands.set(pull.name, pull);
-                    arrayOfCommands.push(pull)
-                    command++
-                } else {
-                    console.log(`${cmds} Command is not Ready`);
-                    continue;
-                }
-                if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
-
-            }
-            client.on("ready", async () => {
-                client.guilds.cache.forEach(async (g) => {
-                    await client.guilds.cache.get(g.id).commands.set(arrayOfCommands);
-                });
-
-
+   try {
+    client.arrayOfCommands = [];
+    let commandcount = 0;
+    fs.readdirSync("./Commands").forEach((cmd) => {
+      let commands = fs
+        .readdirSync(`./Commands/${cmd}/`)
+        .filter((file) => file.endsWith(".js"));
+      for (cmds of commands) {
+        let pull = require(`../Commands/${cmd}/${cmds}`);
+        if (pull.options) {
+          pull.options
+            .filter((g) => g.type === "SUB_COMMAND")
+            .forEach((sub) => {
+              client.subcmd.set(sub.name, sub);
             });
-
-        })
-        console.log(`${command} sls lOADED`);
-    } catch (e) {
+        }
+        if (pull.name) {
+          client.commands.set(pull.name, pull);
+          commandcount++;
+          client.arrayOfCommands.push(pull);
+        } else {
+          continue;
+        }
+        if (pull.aliases && Array.isArray(pull.aliases))
+          pull.aliases.forEach((alias) => client.aliases.set(alias, pull.name));
+      }
+      try {
+        client.on("ready", async () => {
+          await client.application.commands
+            .set(client.arrayOfCommands)
+            .catch((e) => {
+              console.log(e.message);
+            });
+        });
+      } catch (e) {
         console.log(e.message);
-    }
+      }
+    });
+    console.log(`[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+      Loaded ${commandcount} Commands
+    ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]`);
+  } catch (e) {
+    console.log(e);
+  }
 }

6e18f9fb223f2b65a925f85751188222740549d7
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-01-04 23:06:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const { Client, Collection } = require("discord.js");
+const { Client, Collection, Intents } = require('discord.js');
 const fs = require("fs");
 const client = new Client({
-    messageCacheLifetime: 60,
-    fetchAllMembers: false,
-    messageCacheMaxSize: 10,
-    restTimeOffset: 0,
-    restWsBridgetimeout: 100,
-    shards: "auto",
-    allowedMentions: {
-        parse: ["roles", "users", "everyone"],
-        repliedUser: true,
-    },
-    partials: ["MESSAGE", "CHANNEL", "REACTION"],
-    intents: 32767,
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  shards: "auto",
+  allowedMentions: {
+    parse: ["roles", "users", "everyone"],
+    repliedUser: true,
+  },
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    //Intents.FLAGS.GUILD_INTEGRATIONS,
+    //Intents.FLAGS.GUILD_WEBHOOKS,
+    //Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    //Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    //Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    // Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    //Intents.FLAGS.DIRECT_MESSAGE_TYPING
+  ],
 });
 module.exports = client;
 
